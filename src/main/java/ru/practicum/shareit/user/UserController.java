4d3f13fc9c6c6f1e7ecdcfe098178f01621b7816
Maxim <max@MacBook-Pro-Maxim.local>
2026-01-03 23:14:56 +0300

    Реализовано ТЗ sprint 14

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.practicum.shareit.user.UserService;
 import ru.practicum.shareit.user.dto.UserDto;
 
 import java.util.List;
